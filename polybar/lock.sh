116a21a10f5f9f0c11ce379c2a0eb8b890dd64eb
Nhyarlathotep <renaud.melet@epitech.eu>
2018-01-01 19:16:42 +0100

    Add files via upload

--- polybar/lock.sh
+++ polybar/lock.sh
@@ -1,13 +1,48 @@
-#!/bin/sh
+#!/bin/env bash
+
+prelock() {
+        pkill -u "$USER" -USR1 dunst
+}
+
+lock() {
+        #$1 image path
+        letterEnteredColor=d23c3dff
+        letterRemovedColor=d23c3dff
+        passwordCorrect=00000000
+        passwordIncorrect=d23c3dff
+        background=00000000
+        foreground=ffffffff
+        i3lock \
+                -t -n -i "$1" \
+                --timepos="x-90:h-ch+30" \
+                --datepos="tx+24:ty+25" \
+                --clock --datestr "Type password to unlock..." \
+                --insidecolor=$background --ringcolor=$foreground --line-uses-inside \
+                --keyhlcolor=$letterEnteredColor --bshlcolor=$letterRemovedColor --separatorcolor=$background \
+                --insidevercolor=$passwordCorrect --insidewrongcolor=$passwordIncorrect \
+                --ringvercolor=$foreground --ringwrongcolor=$foreground --indpos="x+280:h-70" \
+                --radius=20 --ring-width=4 --veriftext="" --wrongtext="" \
+                --textcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
+                --force-clock
+}
+
+postlock() {
+        pkill -u "$USER" -USR2 dunst
+}
 
 img=/tmp/screenlock.png
 
 scrot -z $img
 convert $img -level 0%,100% \
-	-gamma 0.16,0.15,0.15 \
-	-filter Gaussian -resize 20% -define filter:sigma=0.6 -resize 500% \
-	-font "Noto-Sans" -pointsize 40 -gravity center \
-	~/.config/polybar/doge.png -gravity center -composite \
+	-filter Gaussian \
+        -resize 20% \
+        -define filter:sigma=0.8 \
+	-resize 500% \
 	$img
 
-i3lock -f -i $img
+#	 ~/.config/polybar/doge.png -gravity center -composite \
+#        -font "Noto-Sans" -pointsize 40 -gravity center \
+
+prelock
+lock $img
+postlock
--- polybar/redshift.sh
+++ polybar/redshift.sh
@@ -6,18 +6,18 @@ icon=""
 
 pgrep -x redshift &> /dev/null
 if [[ $? -eq 0 ]]; then
-    #temp=$(redshift -p 2>/dev/null | grep temp | cut -d' ' -f3)#us
-    temp=$(redshift -p 2>/dev/null | grep Temp | cut -d' ' -f4)
+    temp=$(redshift -p 2>/dev/null | grep temp | cut -d' ' -f3)
+    #temp=$(redshift -p 2>/dev/null | grep Temp | cut -d' ' -f4)#us
     temp=${temp//K/}
 fi
 
 #OPTIONAL: Append ' ${temp}K' after $icon
 if [[ -z $temp ]]; then
-    echo "%{u#D08770 +u} %{F#65737E}$icon%{F-} %{u-}"       # Greyed out (not running)
+    echo "%{u#65737E +u} %{F#65737E}$icon%{F-} %{u-}"       # Greyed out (not running)
 elif [[ $temp -ge 5000 ]]; then
-    echo "%{u#D08770 +u} %{F#8FA1B3}$icon%{F-} %{u-}"       # Blue
+    echo "%{u#8FA1B3 +u} %{F#8FA1B3}$icon%{F-} %{u-}"       # Blue
 elif [[ $temp -ge 4000 ]]; then
-    echo "%{u#D08770 +u} %{F#EBCB8B}$icon%{F-} %{u-}"       # Yellow
+    echo "%{u#EBCB8B +u} %{F#EBCB8B}$icon%{F-} %{u-}"       # Yellow
 else
     echo "%{u#D08770 +u} %{F#D08770}$icon%{F-} %{u-}"       # Orange
 fi
